a0f1ca063df8ee906aea31a0e4e380c46309cfbf
Vamsi Yalamanchili <vamsiperumallu@gmail.com>
2021-12-17 15:33:28 -0600

    Rearrange

---
+++ RunLength Enconding/test_RunLengthEncoding.py
@@ -0,0 +1,12 @@
+import RunLengthEncoding    # The code to test
+import unittest   # The test framework
+
+class Test_RunLengthEncoding(unittest.TestCase):
+    def test_case1(self):
+        self.assertEqual(RunLengthEncoding.generateEncoding("aaaabbcc"), "4a2b2c")
+
+    def test_case2(self):
+        self.assertEqual(RunLengthEncoding.generateEncoding("aaaabbcc"), "4a2b2c")
+
+if __name__ == '__main__':
+    unittest.main()
